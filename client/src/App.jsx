a80caa9369dd82cac03e8dc4bace5556067761cb
Pranav <57974336+officialpranav@users.noreply.github.com>
2024-04-22 22:43:09 -0400

    fixed client server connection issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,13 @@ import useSound from 'use-sound'
 
 import { io } from 'socket.io-client'
 
-import { bb, bk, bn, bp, bq, br, wb, wk, wn, wp, wq, wr, move, check, capture, castle, gameOver} from './assets'
-const icons = { bb, bk, bn, bp, bq, br, wb, wk, wn, wp, wq, wr}
-const sounds = { move, check, capture, castle, gameOver}
+import { bb, bk, bn, bp, bq, br, wb, wk, wn, wp, wq, wr, move, check, capture, castle, gameOver } from './assets'
+const icons = { bb, bk, bn, bp, bq, br, wb, wk, wn, wp, wq, wr }
+const sounds = { move, check, capture, castle, gameOver }
 
-const socket = io.connect("http://localhost:3001")
-let numToLetter = ["a","b","c","d","e","f","g","h"]
+const socket = await io.connect("http://localhost:3001")
+
+let numToLetter = ["a", "b", "c", "d", "e", "f", "g", "h"]
 
 function App() {
   const tableEnd = useRef(null)
@@ -21,9 +22,9 @@ function App() {
     gameOver: useSound(sounds.gameOver)[0]
   }
 
-  const [board, setBoard] = useState([])
+  const [board, setBoard] = useState(Array(8).fill([null, null, null, null, null, null, null, null]))
   const [availableMoves, setAvailableMoves] = useState([])
-  const [selectedSquare, setSelectedSquare] = useState('d')
+  const [selectedSquare, setSelectedSquare] = useState('')
   const [turn, setTurn] = useState('')
   const [isCheck, setIsCheck] = useState(false)
   const [isGameOver, setIsGameOver] = useState([false, {
@@ -34,13 +35,10 @@ function App() {
   const [history, setHistory] = useState([])
   const [color, setColor] = useState('')
   const [gameId, setGameId] = useState('')
-  const [player, setPlayer] = useState('')
-  const [opponent, setOpponent] = useState('')
-
-
-
+  const [status, setStatus] = useState('lobby')
+  
   const getMoves = async (square) => {
-    if(true) {
+    if (turn === color) {
       let result = await fetch(`http://localhost:3001/moves?square=${square}`)
       let data = await result.json()
       let moves = data.moves.map(move => move.to)
@@ -60,26 +58,55 @@ function App() {
       }])
       setHistory(data.history)
     }
-  
+
+    const handleTerminate = () => {
+      setStatus('lobby')
+      setGameId('')
+      setBoard(Array(8).fill([null, null, null, null, null, null, null, null]))
+      setAvailableMoves([])
+      setSelectedSquare('')
+      setTurn('')
+      setIsCheck(false)
+      setIsGameOver([false, {
+        isCheckmate: false,
+        isDraw: false,
+        isStalemate: false
+      }])
+      setHistory([])
+      setColor('')
+    }
+
     socket.on('position', handlePosition)
-  
+    socket.on('color', setColor)
+    socket.on('status', setStatus)
+    socket.on('terminate', handleTerminate)
+    socket.on('gameId', setGameId)
+    socket.on('disconnect', () => {
+      handleTerminate()
+    })
+
     return () => {
       socket.off('position', handlePosition)
+      socket.off('color', setColor)
+      socket.off('status', setStatus)
+      socket.off('terminate', handleTerminate)
+      socket.off('disconnect')
+      
     }
   }, [])
 
   useEffect(() => {
-    if(history.length > 0) {
-      let lastMove = history[history.length-1]
+    if (history.length > 0) {
+      let lastMove = history[history.length - 1]
       soundboard[lastMove.type]()
     }
     setSelectedSquare('')
     setAvailableMoves([])
-    tableEnd.current?.scrollIntoView({behavior: 'smooth'})
+    tableEnd.current?.scrollIntoView({ behavior: 'smooth' })
   }, [history])
 
   const movePiece = (move) => {
-    if(true) {
+    if (turn === color) {
       socket.emit('move', move)
     }
   }
@@ -88,8 +115,8 @@ function App() {
   const handleSquareClick = (e) => {
     let square = e.target.getAttribute('square')
 
-    if(selectedSquare !== square) {
-      if(availableMoves.includes(square)) {
+    if (selectedSquare !== square) {
+      if (availableMoves.includes(square)) {
         movePiece(`${selectedSquare}${square}`)
       } else {
         setSelectedSquare(square)
@@ -105,124 +132,91 @@ function App() {
     dragged = e.target.getAttribute('square')
 
     let square = dragged
-    if(selectedSquare !== square) {
+    if (selectedSquare !== square) {
       setSelectedSquare(square)
       getMoves(square)
     }
   }
   const handleDrop = (e) => {
-    e.preventDefault()
-
     let square = e.target.getAttribute('square')
 
-    if(availableMoves.includes(square)) {
+    if (availableMoves.includes(square)) {
       movePiece(`${selectedSquare}${square}`)
     }
   }
 
   return (
     <div className='absolute flex flex-wrap gap-3 items-center justify-center h-full w-full select-none'>
-      <div id="board" className='grid-rows-8 grid-cols-8 grid text-black h-[400px] w-[400px]'>
+      <div className='absolute text-white top-1'>
+      status: {status}<br/>
+      color: {color}<br/>
+      gameId: {gameId}
+    
+      </div>
+      <div id="board" className='grid-rows-8 grid-cols-8 grid grabbable text-black h-[400px] w-[400px]'>
         {board.map((row, i) => row.map((square, j) => {
-          let bgColor = (i+j)%2 === 1 ? 'bg-[#739552]' : 'bg-[#EBECD0]'
-          let textColor = (i+j)%2 === 0 ? 'text-[#739552]' : 'text-[#EBECD0]'
-          return(
-              <div onDrop={handleDrop} onDragOver={(e) => {e.preventDefault()}} className={`relative square cursor-pointer flex flex-col ${bgColor} ${textColor}`} square={`${numToLetter[j]}${8-i}`} onClick={handleSquareClick}>
-                {j===0 && <div square={`${numToLetter[j]}${8-i}`} className='absolute text-xs font-semibold left-[3%]'>{8-i}</div>}
-                {i===7 && <div square={`${numToLetter[j]}${8-i}`} className='absolute text-xs font-semibold self-end right-[5%] top-[69%]'>{numToLetter[j]}</div>}
-                {square != null ? 
-                  <img 
-                    src={icons[`${square.color}${square.type}`]} 
-                    square={`${numToLetter[j]}${8-i}`} 
-                    className='m-auto z-20 h-[90%] w-[90%]'
-                    onDragStart={handleDragStart}
-                    draggable="true"
-                    /> : ""
-                }
-                {squareUnderlay({square: square, coord: `${numToLetter[j]}${8-i}`, history: history, availableMoves: availableMoves, isCheck: isCheck, turn: turn, selectedSquare: selectedSquare})}
-              </div>
-            )
+          let bgColor = (i + j) % 2 === 1 ? 'bg-[#739552]' : 'bg-[#EBECD0]'
+          let textColor = (i + j) % 2 === 0 ? 'text-[#739552]' : 'text-[#EBECD0]'
+          let coord = `${numToLetter[j]}${8 - i}`
+          return (
+            <div onDrop={handleDrop} onDragOver={(e) => { e.preventDefault(); }} className={`relative square flex flex-col ${bgColor} ${textColor}`} square={coord} onClick={handleSquareClick}>
+              {j === 0 && <div square={coord} className='absolute text-xs font-semibold left-[3%]'>{8 - i}</div>}
+              {i === 7 && <div square={coord} className='absolute text-xs font-semibold self-end right-[5%] top-[69%]'>{numToLetter[j]}</div>}
+              {square != null ?
+                <img
+                  src={icons[`${square.color}${square.type}`]}
+                  square={coord}
+                  className='m-auto z-20 h-[90%] w-[90%]'
+                  onDragStart={handleDragStart}
+                  draggable="true"
+                /> : ""
+              }
+              {squareUnderlay({ square: square, coord: coord, history: history, availableMoves: availableMoves, isCheck: isCheck, turn: turn, selectedSquare: selectedSquare })}
+            </div>
+          )
         }))}
         {isGameOver[0] && <div className='absolute bg-white font-black m-20 z-40'>
           game over: {isGameOver[1].isCheckmate ? 'checkmate' : isGameOver[1].isDraw ? 'draw' : isGameOver[1].isStalemate ? 'stalemate' : ''}
         </div>}
       </div>
-      <div className='h-[400px] gap-3 w-96 bg-zinc-700 bg-opacity-90 rounded-xl p-3 flex flex-col'>
-        <div>
-          <p>Player 1</p>
-        </div>
-        <div className='flex flex-col gap-3 grow justify-center'>
-          <div className='h-40 overflow-auto bg-zinc-900 bg-opacity-35 rounded-xl p-2 select-text'>
-            <table className='w-3/5 table-auto'>
-              {history.map((move, i) => {
-                if(i%2 === 0) {
-                  return (
-                    <tr className='text-center  font-semibold text-sm'>
-                      <td className='font-normal text-gray-400'>{i/2 + 1}.</td>
-                      <td>{move.san}</td>
-                      <td>{history[i+1]?.san}</td>
-                    </tr>
-                  )
-                } else {
-                  return 
-                }
-              })}
-            </table>
-            <div ref={tableEnd}/>
-          </div>
-          <button 
-            className=''
-            onClick={() => {
-              socket.emit('reset')
-            }}
-          >reset</button>
-          <button className='' onClick={() => {
-            socket.emit('undo')
-          }}>
-            undo
-          </button>
-        </div>
-        <div>
-          <p>Player 2</p>
-        </div>
-      </div>
+      {panel({history: history, tableEnd: tableEnd, socket: socket, status: status, color: color, gameId: gameId})}
     </div>
   )
 }
 
-
-function squareUnderlay({square, coord, history, availableMoves, isCheck, turn, selectedSquare}){
+//highights squares and displays moves on the board
+function squareUnderlay({ square, coord, history, availableMoves, isCheck, turn, selectedSquare }) {
   let availableMove = null
   let bg = ''
-  if(availableMoves.includes(coord)) {
-    if(square != null) {
+  if (availableMoves.includes(coord)) {
+    if (square != null) {
       availableMove = <div style={{
         border: '4px solid black',
         borderRadius: '50%',
         height: '100%',
         width: '100%',
         opacity: '0.2'
-      }} 
-      square={coord}
+      }}
+        square={coord}
       />
     } else {
-      availableMove = <div square={coord} className='rounded-full bg-black bg-opacity-20 h-[40%] w-[40%]'/>
+      availableMove = <div square={coord} className='rounded-full bg-black bg-opacity-20 h-[40%] w-[40%]' />
     }
   }
 
-  if(history.length > 0) {
-    let lastMove = history[history.length-1]
-    if(coord === lastMove.from || coord === lastMove.to) {
-      bg='bg-yellow-300 bg-opacity-65'
+  if (history.length > 0) {
+    let lastMove = history[history.length - 1]
+    if (coord === lastMove.from || coord === lastMove.to) {
+      bg = 'bg-yellow-300 bg-opacity-65'
     }
   }
-  
-  if(selectedSquare === coord && square != null) {
-    bg='bg-yellow-300 bg-opacity-65'
+
+  if (selectedSquare === coord && square != null) {
+    bg = 'bg-yellow-300 bg-opacity-65'
   }
 
-  if(square != null && square.type === 'k' && isCheck && square.color===turn) {
-    bg='bg-red-600 bg-opacity-70'
+  if (square != null && square.type === 'k' && isCheck && square.color === turn) {
+    bg = 'bg-red-600 bg-opacity-70'
   }
 
   return (
@@ -230,6 +224,102 @@ function squareUnderlay({square, coord, history, availableMoves, isCheck, turn, 
       {availableMove}
     </div>
   )
+}
+
+function controlPanel({history, tableEnd, socket, status, gameId}) {
+  return (
+    <div className='h-[400px] gap-3 w-96 bg-zinc-700 bg-opacity-90 rounded-xl p-3 flex flex-col'>
+      <div>
+        <p>Opponent</p>
+      </div>
+      <div className='flex flex-col gap-3 grow justify-center'>
+        <div className='h-40 overflow-auto bg-zinc-900 bg-opacity-35 rounded-xl p-2 select-text'>
+          <table className='w-3/5 table-auto'>
+            {history.map((move, i) => {
+              if (i % 2 === 0) {
+                return (
+                  <tr className='text-center  font-semibold text-sm'>
+                    <td className='font-normal text-gray-400'>{i / 2 + 1}.</td>
+                    <td>{move.san}</td>
+                    <td>{history[i + 1]?.san}</td>
+                  </tr>
+                )
+              } else {
+                return
+              }
+            })}
+          </table>
+          <div ref={tableEnd} />
+        </div>
+        <div className='grid grid-cols-2 gap-2'>
+          <button className='' onClick={() => {
+            socket.emit('undo', gameId)
+          }}>
+            Undo
+          </button>
+          <button
+            className='px-2'
+            onClick={() => {
+              socket.emit('reset', gameId)
+            }}
+          >Reset Game</button>
+        </div>
+        <button
+          className='px-2'
+          onClick={() => {
+            socket.emit('leave', gameId)
+          }}>
+          Leave
+        </button>
+      </div>
+      <div>
+        <p>You</p>
+      </div>
+    </div>
+  )
+}
+
+function gameJoinPanel({socket, status, color, gameId}) {
+
+  return (
+    <div className='h-[400px] gap-3 w-96 bg-zinc-700 bg-opacity-90 rounded-xl p-3 flex flex-col'>
+      <div>
+        <p className='text-center text-white text-2xl font-bold'>Game Lobby</p>
+      </div>
+      <div className='flex gap-2 text-sm'>
+        <input id="roomInput" className='grow py-1 px-2 rounded-lg' type='text' placeholder='Enter Game Code' />
+        <button
+          className='px-2'
+          onClick={() => {
+            socket.emit('join', document.getElementById('roomInput').value)
+          }}>
+          Join
+        </button>
+        <button
+          className='px-2'
+          onClick={() => {
+            socket.emit('leave', gameId)
+          }}>
+          Leave
+        </button>
+      </div>
+      <div>
+        <p>Color: {color}</p>
+        <p>Status: {status}</p>
+        <p>Game: {gameId}</p>
+      </div>
+    </div>
+  )
+}
+
+//render the correct panel based on the game status
+function panel({history, tableEnd, socket, status, color, gameId}) {
+  //note tableEnd is a ref, i didnt want to rename it cuz id have to refactor :)
+  if(status === 'lobby' || status === 'fail') {
+    return (gameJoinPanel({socket: socket, status: status, color: color, gameId: gameId}))
+  } else {
+    return (controlPanel({history: history, tableEnd: tableEnd, socket: socket, status: status, gameId: gameId}))
+  }
 }
 
 export default App
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ app.use(cors())
 
 const { Chess } = require('chess.js')
 
+let x = 233
 
 const server = http.createServer(app)
 
@@ -17,10 +18,13 @@ const io = new Server(server, {
   }
 })
 
-const chess = new Chess()
-
-const sendPosition = (emitter) => {
+const sendPosition = (emitter, gameId) => {
   let moveType = 'move'
+  if(!games[gameId]) {
+    return
+  }
+  let chess = games[gameId].game
+  
   if(chess.history().length > 0) {
     let lastMove = chess.history({verbose: true})[chess.history().length - 1]
     if(lastMove.flags.includes('k') || lastMove.flags.includes('q')) {
@@ -56,29 +60,134 @@ const sendPosition = (emitter) => {
   })
 }
 
+const games = {}
+const rooms = {}
+
 io.on('connection', (socket) => {
-  sendPosition(socket)
-
-  socket.on('move', (move) => {
-    chess.move(move)
-    sendPosition(io)
+  socket.on('join', (gameId) => {
+    if(!games[gameId]) {
+      socket.join(gameId)
+      rooms[socket.id] = gameId
+      socket.emit('gameId', gameId)
+      //create new game
+      games[gameId] = {
+        game: new Chess(),
+        numPlayers: 1,
+        players: {
+          host: socket.id,
+          opponent: ''
+        },
+        status: 'waiting'
+      }
+      //assign color
+      socket.emit('color', 'white')
+      //send status to room
+      io.to(gameId).emit('status','waiting')
+      sendPosition(io.to(gameId), gameId)
+    } else if(games[gameId]['numPlayers'] === 1){
+      socket.join(gameId)
+      rooms[socket.id] = gameId
+      socket.emit('gameId', gameId)
+      //join existing game
+      games[gameId]['numPlayers'] = 2
+      games[gameId]['players']['opponent'] = socket.id
+      socket.emit('color', 'black')
+      games[gameId].status = 'ready'
+      //send status to room
+      io.to(gameId).emit('status', 'ready')
+      //start the game
+      sendPosition(io.to(gameId), gameId)
+    } else {
+      socket.emit('status', 'fail')
+    }
   })
 
-  socket.on('reset', () => {
-    chess.reset()
-    sendPosition(io)
+  socket.on('move', (move, gameId) => {
+    if(games[gameId].status === 'ready') {
+      games[gameId].game.move(move)
+      sendPosition(io.to(gameId), gameId)
+    }
   })
 
-  socket.on('undo', () => {
-    chess.undo()
-    sendPosition(io)
+  socket.on('reset', (gameId) => {
+    if(games[gameId].status === 'ready') {
+      games[gameId].game.reset()
+      sendPosition(io.to(gameId), gameId)
+    }
+  })
+
+  socket.on('undo', (gameId) => {
+    if(games[gameId].status === 'ready') {
+      games[gameId].game.undo()
+      sendPosition(io.to(gameId), gameId)
+    }
+  })
+
+  socket.on('leave', (gameId) => {
+    //check if socket is host
+    if(games[gameId].players.host === socket.id) {
+      io.in(gameId).fetchSockets().then((sockets) => {
+        for(let socket of sockets) {
+          socket.leave(gameId)
+          delete rooms[socket.id]
+          socket.emit('terminate')
+        }
+      })
+      delete games[gameId]
+      //check is socket is player
+    } else if (games[gameId].players.opponent === socket.id) {
+      games[gameId]['numPlayers'] = 1
+      games[gameId]['players']['opponent'] = ''
+      games[gameId]['status'] = 'waiting'
+      io.to(gameId).emit('status', 'waiting')
+      delete rooms[socket.id]
+      socket.leave(gameId)
+      socket.emit('terminate')
+    }
+  })
+
+  socket.on('disconnect', () => {
+    //check if socket is in a room
+    let gameId = ''
+    if(rooms[socket.id]) {
+      gameId = rooms[socket.id]
+    }
+
+    //if socket is in a room
+    if(gameId !== '') {
+      //if socket is a host
+      if(games[gameId].players.host === socket.id) {
+        //leave room and terminate the game
+        io.to(gameId).emit('terminate')
+        io.in(gameId).fetchSockets().then((sockets) => {
+          for(let socket of sockets) {
+            socket.leave(gameId)
+            socket.emit('gameId', '')
+          }
+        })
+        delete games[gameId]
+      } else if(games[gameId].players.opponent === socket.id){
+        games[gameId]['numPlayers'] = 1
+        games[gameId]['players']['opponent'] = ''
+        games[gameId]['status'] = 'waiting'
+        io.to(gameId).emit('status', 'waiting')
+        socket.leave(gameId)
+      }
+    }
   })
 })
 
 app.get('/moves', (req, res) => {
-  res.send({
-    moves: chess.moves({square: req.query.square, verbose: true})
-  })
+  try {
+    res.send({
+      moves: games[req.query.gameId].game.moves({square: req.query.square, verbose: true})
+    })
+  } catch(err){
+    res.send({
+      moves: [],
+      error: err.message
+    })
+  }
 })
 
 server.listen(3001, ()=>{
